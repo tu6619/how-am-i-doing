5791717cf7e5997d60bd9aa2e7409c6b72594185
franzmoro <franzmoro88@gmail.com>
2016-04-22 16:26:42 +0100

    fixed indentation

--- frontend/src/js/reducers/reducer_userDetails.js
+++ frontend/src/js/reducers/reducer_userDetails.js
@@ -2,9 +2,9 @@ import { GET_USER_DETAILS } from '../actions/actions_index.js'
 
 export default (state = '', action) => {
   switch (action.type) {
-  case GET_USER_DETAILS:
-    return action.payload
-  default:
-    return state
+    case GET_USER_DETAILS:
+      return action.payload
+    default:
+      return state
   }
 }
