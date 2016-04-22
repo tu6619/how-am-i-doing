e61300863223ad658cb90abf14748850c0c44bf8
franzmoro <franzmoro88@gmail.com>
2016-04-22 16:21:40 +0100

    added changeTitle action creator

--- frontend/src/js/actions/actions_index.js
+++ frontend/src/js/actions/actions_index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
-
+// change these consts
 export const GET_USER_DETAILS = 'GET_USER_DETAILS'
+export const CHANGE_TITLE = 'CHANGE_TITLE'
+export const TOGGLE_MENU = 'TOGGLE_MENU'
 
 export const getUserDetails = () => {
   const details = axios.get('/user-request')
