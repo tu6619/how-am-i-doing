346050c95a1574ca12f27cee3d6661ba97149768
franzmoro <franzmoro88@gmail.com>
2016-04-22 16:24:00 +0100

    added toggleMenu action creator and reducer

--- frontend/src/js/actions/actions_index.js
+++ frontend/src/js/actions/actions_index.js
@@ -19,3 +19,8 @@ export const changeTitle = () => {
   }
 }
 
+export const toggleMenu = () => {
+  return {
+    type: TOGGLE_MENU
+  }
+}
--- frontend/src/js/reducers/reducers_index.js
+++ frontend/src/js/reducers/reducers_index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import userDetails from './reducer_userDetails.js'
+import toggleMenu from './reducer_toggleMenu.js'
 import changeTitle from './reducer_changeTitle.js'
 
 const rootReducer = combineReducers({
   userDetails
+  toggleMenu,
   changeTitle,
 })
 
