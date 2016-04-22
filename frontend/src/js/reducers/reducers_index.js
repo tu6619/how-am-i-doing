3a99bf49403f4100236da6cc9d719e9bf1d29c4e
franzmoro <franzmoro88@gmail.com>
2016-04-22 16:24:19 +0100

    react-router-redux routing is part of store

--- frontend/src/js/reducers/reducers_index.js
+++ frontend/src/js/reducers/reducers_index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 import userDetails from './reducer_userDetails.js'
 import toggleMenu from './reducer_toggleMenu.js'
 import changeTitle from './reducer_changeTitle.js'
@@ -7,6 +8,7 @@ const rootReducer = combineReducers({
   userDetails
   toggleMenu,
   changeTitle,
+  routing: routerReducer
 })
 
 export default rootReducer
