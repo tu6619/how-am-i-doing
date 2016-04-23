0dbef880c5403a0e5c9235f2c307b17cb5187a7a
tu6619 <tu6619@my.bristol.ac.uk>
2016-04-23 15:17:59 +0100

    routes file changed to accommodate /user route

--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -5,11 +5,13 @@ import App from './components/App.js'
 import Home from './views/Home/home_index.js'
 import About from './views/About/about_index.js'
 import Contact from './views/Contact/contact_index.js'
+import User from './views/User/user_index.js'
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={Home} />
     <Route path='/about' component={About} />
     <Route path='/contact' component={Contact} />
+    <Route path='/user' component={User} />
   </Route>
 )
