97566b484f8cba0a030804970c88e5677e035fde
tu6619 <tu6619@my.bristol.ac.uk>
2016-04-23 18:19:13 +0100

    routes file changed to include questionnaire

--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -6,12 +6,14 @@ import Home from './views/Home/home_index.js'
 import About from './views/About/about_index.js'
 import Contact from './views/Contact/contact_index.js'
 import User from './views/User/user_index.js'
+import Questionnaire from './views/Questionnaire/questionnaire_index.js'
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={Home} />
-    <Route path='/about' component={About} />
-    <Route path='/contact' component={Contact} />
-    <Route path='/user' component={User} />
+    <Route path='about' component={About} />
+    <Route path='contact' component={Contact} />
+    <Route path='user' component={User} />
+    <Route path='questionnaire' component={Questionnaire} />
   </Route>
 )
