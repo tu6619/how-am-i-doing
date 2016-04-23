2bd6e0f9b889ec552f59cce50b245e0fe127e745
tu6619 <tu6619@my.bristol.ac.uk>
2016-04-23 21:41:52 +0100

    questionnaire view now has a radio form which records 'radioClicked' aspect of buttons

--- frontend/src/js/views/Questionnaire/questionnaire_index.js
+++ frontend/src/js/views/Questionnaire/questionnaire_index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// import ReactDOM from 'react-dom'
 import { Modal, Button } from 'react-bootstrap'
 
 class Questionnaire extends React.Component {
@@ -6,10 +7,14 @@ class Questionnaire extends React.Component {
     super()
     this.state = {
       showModal: false,
-      sent: false
+      answers: [],
+      sent: false,
+      modalNumber: 0
     }
     this.close = this.close.bind(this)
     this.open = this.open.bind(this)
+    this.next = this.next.bind(this)
+    this.clickRadio = this.clickRadio.bind(this)
   }
 
   close () {
@@ -20,7 +25,27 @@ class Questionnaire extends React.Component {
     this.setState({ showModal: true })
   }
 
+  next (e) {
+    console.log(this.refs.result)
+    const modalNumber = this.state.modalNumber
+    this.setState({
+      answers: this.state.answers.concat([this.state.radioClicked]),
+      modalNumber: modalNumber + 1
+    })
+
+    // [0, 1, 2, 3].map(i => this.refs[i].value)
+  }
+
+  clickRadio (i) {
+    this.setState({
+      radioClicked: i
+    })
+  }
+
   render () {
+    console.log(this.state)
+    const { clickRadio } = this
+    const { radioClicked } = this.state
     return (
     <div>
       <Button className='start-questionnaire' bsStyle='primary' bsSize='large' onClick={this.open}>
@@ -29,15 +54,41 @@ class Questionnaire extends React.Component {
       <Modal show={this.state.showModal} onHide={this.close}>
         <Modal.Header closeButton>
           <Modal.Title>
-            Resources
+            I feel sad or empty ... {this.state.modalNumber}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          hello
+        <form>
+          <input
+            type='radio'
+            name='gender'
+            value= 'male'
+            onClick={() => clickRadio(0)}
+          /> male
+          <br />
+          <input
+            type='radio'
+            name='gender'
+            value= 'male'
+            onClick={() => clickRadio(1)}
+          /> Male<br />
+          <input
+            type='radio'
+            name='gender'
+            value= 'male'
+            onClick={() => clickRadio(2)}
+          /> Male<br />
+          <input
+            type='radio'
+            name='gender'
+            value= 'male'
+            onClick={() => clickRadio(3)}
+          /> Male<br />
+        </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={this.close}>
-            Close
+          <Button onClick={this.next}>
+            Next
           </Button>
         </Modal.Footer>
       </Modal>
