a9b1b32ce7bf390a28ae751cb961db3dcc07cd43
tu6619 <tu6619@my.bristol.ac.uk>
2016-04-23 15:19:55 +0100

    user_index file created for /user view. contains buttons to fill in the questionnaire and to the visualisation views

---
+++ frontend/src/js/views/User/user_index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Grid, Row, Col, Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { getUserDetails, changeTitle } from '../../actions/actions_index.js'
+
+class User extends React.Component {
+  render () {
+    return (
+      <Grid className='user'>
+        <h1>How am I doing?</h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere.</p>
+        <Row>
+          <Col sm={6} md={3}>
+            <Button className='visualise' bsSize='large'>VISUALISE</Button>
+          </Col>
+          <Col sm={6} md={3}>
+            <Button className='questionnaire' bsSize='large'>QUESTIONNAIRE</Button>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
+
+User.propTypes = {
+  title: React.PropTypes.string,
+  userDetails: React.PropTypes.string,
+  changeTitle: React.PropTypes.func
+}
+
+const mapStateToProps = (state) => {
+  return {
+    userDetails: state.userDetails,
+    title: state.changeTitle.title
+  }
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({getUserDetails, changeTitle}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(User)
