dbec8b38f6581ebdf9b3d027c9b6e83c5ab41d04
tu6619 <tu6619@my.bristol.ac.uk>
2016-04-24 16:04:16 +0100

    filled line graph styling changed

--- frontend/src/js/components/chart.js
+++ frontend/src/js/components/chart.js
@@ -131,7 +131,7 @@ const createDisaggregatedChartRenderer = type => (_data) => {
       x: xAxis,
       y,
       type,
-      fill: 'tonexty',
+      fill: 'tonextx',
       mode: 'none'
     }))
 
