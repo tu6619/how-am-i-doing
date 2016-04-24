2e91d84d4227836f2126e27da9618dcd4c2bc90b
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-04-24 13:48:52 +0100

    :art: cleaning ..

--- dev_server.js
+++ dev_server.js
@@ -1,17 +1,15 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.development.config.js');
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.development.config.js')
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
-  proxy: {
-    "*": "http://localhost:4000"
-  }
+  proxy: { "*": "http://localhost:4000" }
 }).listen(8080, 'localhost', function (err, result) {
   if (err) {
-    return console.log(err);
+    return console.log (err)
   }
-  console.log('Listening at http://localhost:8080/');
-});
+  console.log('Listening at http://localhost:8080/')
+})
