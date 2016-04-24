096b8a121469bb1aabc5e2187acf2b63f55902bc
tu6619 <tu6619@my.bristol.ac.uk>
2016-04-24 12:45:33 +0100

    questionnaire view / modal now has a form with radio buttons that work and collect data about the user's emotions / feelings. ajax request using axios in progress.

--- frontend/src/js/views/Questionnaire/questionnaire_index.js
+++ frontend/src/js/views/Questionnaire/questionnaire_index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Axios from 'axios'
 // import ReactDOM from 'react-dom'
 import { Modal, Button } from 'react-bootstrap'
 
@@ -15,6 +16,8 @@ class Questionnaire extends React.Component {
     this.open = this.open.bind(this)
     this.next = this.next.bind(this)
     this.clickRadio = this.clickRadio.bind(this)
+    this.clear = this.clear.bind(this)
+    this.send = this.send.bind(this)
   }
 
   close () {
@@ -26,14 +29,20 @@ class Questionnaire extends React.Component {
   }
 
   next (e) {
-    console.log(this.refs.result)
     const modalNumber = this.state.modalNumber
     this.setState({
       answers: this.state.answers.concat([this.state.radioClicked]),
       modalNumber: modalNumber + 1
     })
+  }
 
-    // [0, 1, 2, 3].map(i => this.refs[i].value)
+  clear () {
+    document.getElementById('clear-button').addEventListener('click', () => {
+      ['a', 'b', 'c', 'd'].forEach((id) => {
+        document.getElementById(id).checked = false
+      })
+      return false
+    })
   }
 
   clickRadio (i) {
@@ -42,9 +51,26 @@ class Questionnaire extends React.Component {
     })
   }
 
+  send () {
+    const data = {
+      questions: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      answers: this.state.answers
+    }
+    Axios.post('/data.json', { data })
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => console.log(err))
+  }
+
   render () {
-    console.log(this.state)
+    // array of questions
+    const questions = ['I feel sad or empty ...', 'Nothing is fun much anymore ...', 'I have trouble sleeping ...',
+    'I have problems with my appetite ...',
+    'I have no energy for things ...', 'I am tired a lot ...', 'I cannot think clearly ...',
+    'I feel worthless ...', 'I feel like I don\'t want to move ...', 'I feel restless ...']
     const { clickRadio } = this
+    const { clear } = this
     const { radioClicked } = this.state
     return (
     <div>
@@ -54,42 +80,63 @@ class Questionnaire extends React.Component {
       <Modal show={this.state.showModal} onHide={this.close}>
         <Modal.Header closeButton>
           <Modal.Title>
-            I feel sad or empty ... {this.state.modalNumber}
+            {questions[this.state.modalNumber]}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
         <form>
           <input
+            id='a'
             type='radio'
             name='gender'
-            value= 'male'
-            onClick={() => clickRadio(0)}
-          /> male
-          <br />
+            onClick={() => {
+              clickRadio(0)
+              clear()
+            }
+          }
+            /> Never <br />
           <input
+            id='b'
             type='radio'
             name='gender'
-            value= 'male'
-            onClick={() => clickRadio(1)}
-          /> Male<br />
+            onClick={() => {
+              clickRadio(1)
+              clear()
+            }
+          }
+          /> Sometimes <br />
           <input
+            id='c'
             type='radio'
             name='gender'
-            value= 'male'
-            onClick={() => clickRadio(2)}
-          /> Male<br />
+            onClick={() => {
+              clickRadio(2)
+              clear()
+            }
+          }
+          /> Often <br />
           <input
+            id='d'
             type='radio'
             name='gender'
-            value= 'male'
-            onClick={() => clickRadio(3)}
-          /> Male<br />
+            onClick={() => {
+              clickRadio(3)
+              clear()
+            }
+          }
+          /> Always <br />
         </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={this.next}>
-            Next
-          </Button>
+          {this.state.modalNumber === questions.length - 1 ? (
+            <Button id='clear-button' onClick={this.send}>
+              Submit
+            </Button>
+          ) : (
+            <Button id='clear-button' onClick={this.next}>
+              Next
+            </Button>
+          )}
         </Modal.Footer>
       </Modal>
     </div>
